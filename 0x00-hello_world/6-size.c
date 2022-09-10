1f858caa4cf29522658673688802b2fa6b49a81a
Helianthus22 <phidenna0@gmail.com>
2022-09-09 17:34:38 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 
+
 /**
- *  * main - pint the size of various types
- *
- *   *
- *
- *   * Description: prints the size of various types on the computer it is compiled
- *
- *   *
- *
- *   * and run on
+ *  * main - Entry point
  *
  *   *
  *
@@ -17,20 +10,19 @@
  *
  */
 
-
 int main(void)
 
 {
-	int a;	
-	long int b;	
-	long long int c;		
-	char d;			
-	float f;
 
-	printf("size of a char: %d byte(s)\n", sizeof(char));
-	printf("size of an int: %d byte(s)\n", sizeof(int));
-	printf("size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("size of a float: %d byte(s)\n", sizeof(float));
-	return (0);
-}
+		int a;
+		long int b;
+		long long int c;
+		char d;
+		float f;
+
+		printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+		printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));		
+		printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));		
+		printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));					     printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));		
+		return (0);
+}		
