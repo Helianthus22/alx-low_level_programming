179af448e7615fe4de9d87f1ea0f6e6458257bff
Helianthus22 <phidenna0@gmail.com>
2022-09-13 20:56:38 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,9 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char 0)
+#include<stdio.h>
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -14,6 +16,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-void num(int r, int n)
 
-#endif
+#endif /* MAIN_H */
