9bf93ed2eb38bfca3a2fda082a1f5bac7895d567
Helianthus22 <phidenna0@gmail.com>
2022-09-13 21:38:26 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,24 +2,18 @@
 
 
 /**
- *  * print_alphabet_x10 - prints alphabets 10
- *   *
- *    * Description: prints 10 times
- *
- *     * Return: Always(0).
- *
+ *  print_alphabet_x10 - prints alphabets 10
+ *  Description: prints 10 times
+ *  Return: Always(0).
  */
 
-
 void print_alphabet_x10(void)
-
 {
 
 		int alphabet_count = 0;
 
 		
 		while (alphabet_count < 10)
-
 		{
 			char ch = 'a';
 		
