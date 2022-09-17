6d6114ba892778c96590a88ad5553603dcb54931
Helianthus22 <phidenna0@gmail.com>
2022-09-16 18:04:16 -0700

    positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: printvalueofnstatus;zero,positiveornegative
- * Return: Always 0 (Success)
+ *  main - Entry point
+ *  Description: print value of n satus; zero, positive or negative
+ *  Return: Always 0 (Success)
  */
 
-voidpositive_or_negative(int)
+void positive_or_negative(int i)
 {
-	/*myifstatement*/
-	if(i==0)
-	{
-		printf("%diszero\n",i);
-	}
-	elseif(i>0)
-	{
-		printf("%dispositive\n",i);
-	}
-	else
-	{
-		printf("%disnegative\n",i);
-	}
+
+		/* my if statement */
+		if (i == 0)
+		{
+			printf("%d is zero\n", i);
+		}
+		else if (i > 0)
+		{
+			printf("%d is positive\n", i);
+		}
+		else
+		{
+			printf("%d is negative\n", i);
+		}
 }
