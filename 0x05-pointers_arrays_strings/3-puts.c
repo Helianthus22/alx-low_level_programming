556b5b885bc4c5c2f9addd6695ed3e32cd0f02f9
Helianthus22 <phidenna0@gmail.com>
2022-09-19 16:13:26 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts - prints astring to stdout
+ * @str - a string to be printed
+ * Return void
+ */
+void _puts(char *str)
+{
+	char s;
+
+	for (s = 0 *(str + s) != '\0'; s++)
+	_putchar(*(str + s));
+	_putchar('\n');
+}
