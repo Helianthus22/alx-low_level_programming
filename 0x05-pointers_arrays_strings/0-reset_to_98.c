253e141f6f326b88a76184fe370b64c54b3d9270
Helianthus22 <phidenna0@gmail.com>
2022-09-19 12:06:20 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+/**
+ * main - updates value of pointer to 98
+ * *n: input to update to 98
+ */
+
+void reset_to_98(int *n)
+{
+	int var = 98;
+	int *n = &var;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+A directory that explains pointers, arrays, strings, scopes of variables and how to use and manipulate them.
+
